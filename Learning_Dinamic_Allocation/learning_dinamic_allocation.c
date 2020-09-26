9bd2161e57f80b6759df4b668c6699196e9369b5
LuizHenriqueLobo1 <luizhenriquewolf@gmail.com>
2020-09-26 19:11:47 -0300

    Commitando novo programa em C

---
+++ Learning_Dinamic_Allocation/learning_dinamic_allocation.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    
+    int *p;
+    int i;
+    int qtdElementos;
+
+    printf("Digite a quantidade de elementos: ");
+    scanf("%d", &qtdElementos);
+
+    p = (int *)(malloc(qtdElementos * sizeof(int)));
+
+    if(p == NULL) {
+        printf("ERRO!!!");
+        system("pause");
+    }
+
+    printf("Digite os elementos: ");
+    for(i = 0 ; i < qtdElementos ; i++) {
+        scanf("%d", &p[i]);
+    }
+
+    for(i = 0 ; i < qtdElementos ; i++) {
+        printf("Elemento -> %d", p[i]);
+    }
+
+    return 0;
+}
