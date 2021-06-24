18bcfacc41de4f2ae45a33ec1e2236a5cd8a42e6
LuizHenriqueLobo1 <luizhenriquelobo14@gmail.com>
2021-06-23 21:05:54 -0300

    Adicionando menu e modularizando projeto

--- Projeto_com_Arquivos/arquivos_main.c
+++ Projeto_com_Arquivos/arquivos_main.c
@@ -16,15 +16,84 @@ typedef struct {
 	notas provas;
 }dados;
 
-void mediaAlunos(dados pes[]);
+int menu();
+int cadastrarAlunosAutomaticamente(dados pessoa[]);
+void imprimirAlunosCadastrados(dados pessoa[]);
+void mediaAlunos(dados pessoa[]);
 
 int main() {
 
-	FILE *file;
 	dados pessoa[TAM];
-	int i;
+	int loop = 1;
+	int opcao = 0;
+	int alunosCadastrados = 0;
+
+	while(loop == 1) {
+
+		opcao = menu();
+
+		switch(opcao) {
+
+			case 0: {
+				printf("\nPROGRAMA ENCERRADO\n");
+				loop = 0;
+				break;
+			}
+
+			case 1: {
+				alunosCadastrados = cadastrarAlunosAutomaticamente(pessoa);
+				printf("\nALUNOS CADASTRADOS COM SUCESSO\n");
+				break;
+			}
+
+			case 2: {
+				if(alunosCadastrados == 1) 
+					imprimirAlunosCadastrados(pessoa);
+				else
+					printf("\nNAO EXISTE ALUNO CADASTRADO\n");
+				break;
+			}
+
+			case 3: {
+				if(alunosCadastrados == 1)
+					mediaAlunos(pessoa);
+				else
+					printf("\nNAO EXISTE ALUNO CADASTRADO\n");
+				break;
+			}
+
+			default: {
+				break;
+			}
+
+		}
+
+	}
+
+	return 0;
+}
+
+int menu() {
+
+	int opcao = 0;
+
+	printf("\n----------------- MENU -----------------\n");
+	printf("0. SAIR\n");
+	printf("1. CADASTRAR ALUNOS AUTOMATICAMENTE\n");
+	printf("2. IMPRIMIR ALUNOS CADASTRADOS\n");
+	printf("3. IMPRIMIR MEDIA DOS ALUNOS CADASTRADOS\n");
+	printf("----------------------------------------\n");
+	printf("> ");
+	scanf("%d", &opcao);
+
+	return opcao;
+}
+
+int cadastrarAlunosAutomaticamente(dados pessoa[]) {
+
+	FILE *file;
 	char getbr;
-	int media[TAM];
+	int i;
 
 	file = fopen("dados.txt", "r");
 
@@ -38,6 +107,15 @@ int main() {
 		fscanf(file, "%c", &getbr);
 	}
 
+	fclose(file);
+
+	return 1;
+}
+
+void imprimirAlunosCadastrados(dados pessoa[]) {
+
+	int i;
+
 	for(i = 0; i < TAM; i++) {
 		printf("\nNome: %s", pessoa[i].nome);
 		printf("Sexo: %c", pessoa[i].sexo);
@@ -46,20 +124,17 @@ int main() {
 		printf("\nNota 2: %.2f", pessoa[i].provas.nota2);
 		printf("\nNota 3: %.2f\n", pessoa[i].provas.nota3);
 	}
-
-	mediaAlunos(pessoa);
-
-	fclose(file);
-	return 0;
 }
 
-void mediaAlunos(dados pes[]) {
+void mediaAlunos(dados pessoa[]) {
 
 	float media;
 	int i;
 
 	for(i = 0; i < TAM; i++) {
-		media = (pes[i].provas.nota1 + pes[i].provas.nota2 + pes[i].provas.nota3) / 3;
+		media = (pessoa[i].provas.nota1 + pessoa[i].provas.nota2 + pessoa[i].provas.nota3) / 3;
 		printf("\nMedia: %.2f", media);
 	}
+
+	printf("\n");
 }
