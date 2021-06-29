6fcf6e83c2103c3ff00f5011ad43dc0775a4ce7c
LuizHenriqueLobo1 <luizhenriquelobo14@gmail.com>
2021-06-29 18:51:07 -0300

    Adicionando menu e usuário escolhe qtd números gerados

--- Gerador_Números_Aleatórios_em_C/gerador_numeros.c
+++ Gerador_Números_Aleatórios_em_C/gerador_numeros.c
@@ -2,23 +2,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int menu();
+void numerosAleatorios(int qtd);
+
 int main() {
+        
+    int loop = 1;
+    int opcao;
+    int qtdNumeros;
+
+    while(loop == 1) {
+
+        opcao = menu();
+
+        switch(opcao) {
+
+            case 0: {
+                printf("\nPROGRAMA ENCERRADO\n");
+                loop = 0;
+                break;
+            }
+
+            case 1: {
+                printf("\nDIGITE QUANTOS NUMEROS QUER GERAR > ");
+                scanf("%d", &qtdNumeros);
+
+                numerosAleatorios(qtdNumeros);
+            }
+
+        }
+
+    }
+
     
+
+    return 0;
+}
+
+void numerosAleatorios(int qtd) {
+
     int i;
-    int num[5];
+    int num[qtd];
     int positivo = 0;
     int negativo = 0;
 
     srand(time(NULL));
 
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < qtd; i++) {
         num[i] = (rand() % 21);
     }
 
     printf("\n");
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < qtd; i++) {
         printf("N %d: %d\n", i+1, num[i]);
     }
+}
 
-    return 0;
+int menu() {
+    
+    int opcao;
+
+    printf("\n---------- MENU -----------\n");
+    printf("0. SAIR\n");
+    printf("1. GERAR NUMEROS ALEATORIOS\n");
+    printf("---------------------------");
+    printf("\n> ");
+    scanf("%d", &opcao);
+
+    return opcao;
 }
